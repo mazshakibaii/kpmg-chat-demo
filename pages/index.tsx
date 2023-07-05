17d242576ffc5e2231e56f24653dd640a70003ab
Maz <mazshakibaii@gmail.com>
2023-07-05 22:33:12 +0000

    Updated index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,33 @@
-import { Center, Text } from '@mantine/core'
+import ChatBubble from "@/components/ChatBubble";
+import { Card, Center, Stack, Text, TextInput } from "@mantine/core";
+import { useForm } from "@mantine/form";
 
-export default function Home() {
+export default function Chat() {
+  const form = useForm({
+    initialValues: {
+      url: "",
+    },
+  });
+
+  const url = form.getInputProps("url").value;
+
   return (
-    <Center h="100dvh" w="100dvw">
-      <Text>Hello</Text>
-    </Center>
-  )
+    <>
+      <Center w="100dvw" h="100dvh">
+        <Card w={600}>
+          <Stack>
+            <form>
+              <TextInput
+                w="100%"
+                placeholder="Enter website address"
+                label="Chat to any web page!"
+                {...form.getInputProps("url")}
+              />
+            </form>
+          </Stack>
+        </Card>
+      </Center>
+      {url && <ChatBubble webUrl={url} />}
+    </>
+  );
 }
