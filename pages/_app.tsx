a9e21fbe874a11a6fb46901f8210960530f4bcba
Maz <mazshakibaii@gmail.com>
2023-07-06 12:48:48 +0000

    Updated title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,28 @@
-import { AppProps } from 'next/app';
-import Head from 'next/head';
-import { MantineProvider } from '@mantine/core';
+import { AppProps } from "next/app";
+import Head from "next/head";
+import { MantineProvider } from "@mantine/core";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <>
-  <Head>
-    <title>KPMG - AI Document Chate</title>
-    <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-  </Head>
+  return (
+    <>
+      <Head>
+        <title>KPMG - AI Document Chat</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
 
-  <MantineProvider
-    withGlobalStyles
-    withNormalizeCSS
-    theme={{
-      /** Put your mantine theme override here */
-      colorScheme: 'dark',
-    }}
-  >
-    <Component {...pageProps} />
-  </MantineProvider>
-</>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: "dark",
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
 }
