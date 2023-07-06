2016f6363287409b7688b0dc1e525a75cd6a2b31
Maz <mazshakibaii@gmail.com>
2023-07-06 12:33:45 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import { MantineProvider } from '@mantine/core';
 export default function App({ Component, pageProps }: AppProps) {
   return <>
   <Head>
-    <title>Page title</title>
+    <title>KPMG - AI Document Chate</title>
     <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
   </Head>
 
