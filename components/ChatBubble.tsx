1dbf76c3ae768d5dd91d51ee7bce8f0935e38d82
Maz <mazshakibaii@gmail.com>
2023-07-06 09:08:12 +0000

    Updated system prompt

--- components/ChatBubble.tsx
+++ components/ChatBubble.tsx
@@ -13,6 +13,8 @@ function ChatBubble({ webUrl }: urlString) {
       chatflowConfig={{
         topK: 4,
         url: webUrl,
+        systemMessagePrompt:
+          "You are an AI assistant tasked with answering questions based on a given context. You must ONLY answer using the information provided in the context. If the context doesn't answer the user's question, you must respond saying you're unsure.",
       }}
     />
   );
